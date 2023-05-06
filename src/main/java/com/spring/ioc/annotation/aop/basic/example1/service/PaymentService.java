4b7d03087209044e0fd6e6a60badfa00dc2a0fc8
Sunny <117725286+WickedSunny427@users.noreply.github.com>
2023-05-06 16:05:40 +0530

    Clean Up

--- src/main/java/com/spring/ioc/annotation/aop/basic/example1/service/PaymentService.java
+++
@@ -1,5 +0,0 @@
-package com.spring.ioc.di.annotation.aop.basic.example1.service;
-
-public interface PaymentService {
-	public void makePayment();
-}
--- src/main/java/com/spring/ioc/annotation/componentscan/Configuration.java
+++
@@ -1,8 +0,0 @@
-package com.spring.ioc.di.annotation.componentscan;
-
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan("com.spring.ioc.di.annotation.jsr250.inject")
-public class Configuration {
-
-}
